2d4c831da156eb8eed9351fdd8d0c1523a2cf1db
Johan Haleby <johan.haleby@gmail.com>
2017-02-16 07:29:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+NodeJS Starter
+==============
+
+Start project for nodejs lab
+
+
+### Development
+
+1. npm install
+1. npm run-script watch (starts server and allows you to do F5 in the server on changes)
+
+Now navigate to [http://localhost:3000](http://localhost:3000)
---
+++ server.js
@@ -0,0 +1,34 @@
+    'use strict';
+
+var log = require('./lib/logger.js');
+var express = require('express');
+var app = express();
+var path = require('path');
+var api = require('./http/api.js');
+var config = require('config');
+
+function shutdown() {
+    log.info('About to exit.');
+}
+
+process.on('SIGTERM', shutdown);
+process.on('exit', shutdown);
+process.on('uncaughtException', shutdown);
+
+// Static resources
+app.use('/resources', express.static(__dirname + '/resources'));
+
+// API
+app.use('/api', api);
+
+// HTML
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+var server = app.listen(3000, function () {
+    log.info('NodeJS HTTP server is listening on port 3000! (env %s)', process.env.NODE_ENV == undefined ? "default" : process.env.NODE_ENV)
+});
+
+module.exports = app;
